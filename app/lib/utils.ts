d69953838203a2a47c70579df0bcabdd18c5ed1f
Ariel Chura <ariel.chura.c@gmail.com>
2023-11-03 14:16:05 -0400

    feat(utils): add date utils

---
+++ app/lib/utils.ts
@@ -0,0 +1,24 @@
+export const formatDate = (date: string) => {
+  const dateObj = new Date(date);
+  const month = dateObj.toLocaleString('default', {month: 'short'});
+  const day = dateObj.getDate();
+  const year = dateObj.getFullYear();
+  return `${day} de ${month}, ${year}`;
+};
+
+export const formatFullDate = (date: string | Date) => {
+  const dateObj = new Date(date);
+  const month = dateObj.toLocaleString('default', {month: 'short'});
+  const day = dateObj.getDate();
+  const year = dateObj.getFullYear();
+  const hours = dateObj.getHours();
+  const minutes = dateObj.getMinutes();
+  return `${day} de ${month}, ${year} a las ${hours}:${minutes}`;
+};
+
+export const addYearToDate = (date: Date | string): Date => {
+  const dateObj = new Date(date);
+  const year = dateObj.getFullYear();
+  dateObj.setFullYear(year + 1);
+  return dateObj;
+};
