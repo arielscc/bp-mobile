ca2c4096d25d70d68dd5fd0cd2e86a89860a80ab
Ariel Chura <ariel.chura.c@gmail.com>
2023-11-03 13:26:29 -0400

    chore(colors): add base colors

---
+++ app/ui/colors.ts
@@ -0,0 +1,10 @@
+export const Colors = {
+  black: '#1f2937',
+  blue: '#262d5b',
+  gray: '#94a3b8',
+  white: '#ffffff',
+  yellow: '#f4dc32',
+  red: '#ef4444',
+  tomato: '#ff6347',
+  neutral: '#334155',
+};
