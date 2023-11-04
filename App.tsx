413a331589a4589c77619f2197729c604450e014
Ariel Chura <ariel.chura.c@gmail.com>
2023-11-04 04:01:18 -0400

    feat(splash): add SplashScreen to hide on app launch

--- App.tsx
+++ App.tsx
@@ -1,13 +1,18 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
-import React from 'react';
+import React, {useEffect} from 'react';
 import 'react-native-gesture-handler';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import SplashScreen from 'react-native-splash-screen';
 import {ProductsProvider} from './app/context/provider/ContextProvider';
 import {AppNavigation} from './app/navigation/navigation';
 
 function App(): JSX.Element {
   const client = new QueryClient();
+
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
 
   return (
     <ProductsProvider>
